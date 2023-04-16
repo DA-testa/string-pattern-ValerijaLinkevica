f35e385a58bc64dcaee946b1ee867e015ec6aa38
ValerijaLinkevica <124578635+ValerijaLinkevica@users.noreply.github.com>
2023-04-16 20:06:27 +0000

    "Order in the court room"

--- hash_substring.py
+++ hash_substring.py
@@ -13,7 +13,6 @@ def read_input():
         text = lines[1].rstrip()
 
     elif "I" in choice:
-        # input from keyboard
         pattern = input().rstrip()
         text = input().rstrip()
 
@@ -25,23 +24,24 @@ def print_occurrences(output):
 
 
 def get_occurrences(pattern, text):
+    q = 7
     indexes = []
     alphabet_len = 256
     pattern_len = len(pattern)
     text_len = len(text)
     pattern_hash_val = 0
     text_hash_val = 0
-    i = 0
-    j = 0
-    q = 7
-    h = 1
-
-    for i in range(pattern_len - 1):
-        h = (h * alphabet_len) % q
 
     for i in range(pattern_len):
         pattern_hash_val = (alphabet_len * pattern_hash_val + ord(pattern[i])) % q
         text_hash_val = (alphabet_len * text_hash_val + ord(text[i])) % q
+
+    i = 0
+    j = 0
+    h = 1
+
+    for i in range(pattern_len - 1):
+        h = (h * alphabet_len) % q
 
     for i in range(text_len - pattern_len + 1):
         if pattern_hash_val == text_hash_val:
